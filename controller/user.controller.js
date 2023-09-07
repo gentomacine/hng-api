4a11fd16319376d0d35a9fb26e0fec596068a246
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2023-09-07 23:57:18 +0100

    okay

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,64 +1,72 @@
 import User from "../model/user.model.js";
 
 export const createUser = async (req, res) => {
-    const { slack_name, track, current_day, github_file_url, github_repo_url, utc_time } = req.body;
-  
-    try {
-      // Check if a user with the same slackname and track already exists
-      const foundUser = await User.findOne({ slack_name, track });
-  
-      if (foundUser) {
-        return res.status(409).json({ message: "User already exists" });
-      };
-    
-  
-      // Create a new user
-      const newUser = await User.create({
-        slack_name,
-        track,
-        current_day,
-        github_file_url,
-        github_repo_url,
-        utc_time,
-      });
-  
-      res.status(201).json({
-        status: "success",
-        data: newUser,
-      });
-    } catch (error) {
-      // Handle errors and provide a more informative response
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-  export const getUserInfo = async (req, res) => {
-    const { slack_name, track } = req.query; // Retrieve values from query parameters
-  
-    try {
-      // Find the user that matches both slackname and track criteria
-      const user = await User.findOne({ slack_name, track });
-  
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      // Ensure utc_time is a Date object (parse it if it's not)
-    const utcTime = user.utc_time instanceof Date ? user.utc_time : new Date(user.utc_time);
+  const {
+    slack_name,
+    track,
+    current_day,
+    github_file_url,
+    github_repo,
+    utc_time,
+  } = req.body;
 
-    // Format the UTC time to the desired format (e.g., "2023-09-07T21:46:17Z")
-    const formattedUtcTime = utcTime.toISOString();
-  
-      res.status(200).json({
-        slack_name: user.slack_name,
-        track: user.track,
-        current_day: user.current_day,
-        github_file_url: user.github_file_url,
-        github_repo_url: user.github_repo_url,
-        utc_time: formattedUtcTime,
-        status_code: 200,
-      });
-    } catch (error) {
-      // Handle errors and provide a more informative response
-      res.status(500).json({ message: error.message });
+  try {
+    // Check if a user with the same slackname and track already exists
+    const foundUser = await User.findOne({ slack_name, track });
+
+    if (foundUser) {
+      return res.status(409).json({ message: "User already exists" });
     }
-  };
+
+    // Parse the utc_time string into a Date object
+    const parsedUtcTime = new Date(utc_time);
+
+    // Create a new user with the parsed UTC time
+    const newUser = await User.create({
+      slack_name,
+      track,
+      current_day,
+      github_file_url,
+      github_repo,
+      utc_time: parsedUtcTime, // Include the parsed UTC time
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: newUser,
+    });
+  } catch (error) {
+    // Handle errors and provide a more informative response
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getUserInfo = async (req, res) => {
+  const { slack_name, track } = req.query; // Retrieve values from query parameters
+
+  try {
+    // Find the user that matches both slackname and track criteria
+    const user = await User.findOne({ slack_name, track });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Format the date without milliseconds
+    const formattedUtcTime = user.utc_time.replace(/\.\d{3}Z$/, 'Z');
+
+    res.status(200).json({
+      slack_name: user.slack_name,
+      track: user.track,
+      current_day: user.current_day,
+      github_file_url: user.github_file_url,
+      github_repo: user.github_repo,
+      utc_time: formattedUtcTime, // Use the formatted date without milliseconds
+      status_code: 200,
+    });
+  } catch (error) {
+    // Handle errors and provide a more informative response
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- model/user.model.js
+++ model/user.model.js
@@ -1,40 +1,51 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        slack_name: {
-            type: String,
-            required: [true, "slackname is needed"]
-        },
-        
-        track: {
-            type: String,
-            required: [true, "track is needed"]
-        },
-
-        current_day: {
-            type: String, // Change the field type to String
-            enum: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // Use an enum to specify the days of the week
-            required: [true, "currentDay is needed"]
-        },
-
-        utc_time: {
-                type: Date,
-                default: Date.now,
-                get: (v) => v.toISOString() // Convert the Date to ISO8601 format
-        },
-
-        github_file_url: {
-            type: String
-        },
-        github_repo_url: {
-            type: String
-        },
+  {
+    slack_name: {
+      type: String,
+      required: [true, "slackname is needed"],
     },
-    {
-        timestamps: true,
-        toJSON: { virtuals: true }
-    }
+
+    track: {
+      type: String,
+      required: [true, "track is needed"],
+    },
+
+    current_day: {
+      type: String,
+      enum: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      required: [true, "currentDay is needed"],
+    },
+
+    utc_time: {
+      type: Date,
+      default: Date.now,
+      get: function () {
+        const date = new Date(this.createdAt);
+        return new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString().slice(0, -1) + "Z";
+      },
+    },
+
+    github_file_url: {
+      type: String,
+    },
+    github_repo_url: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
 );
 
 const User = mongoose.model("User", userSchema);
