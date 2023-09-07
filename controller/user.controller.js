e6ad6e6147e521dfc3fb063181ea0c3dab476163
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2023-09-07 22:56:22 +0100

    okay

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,7 +9,8 @@ export const createUser = async (req, res) => {
   
       if (foundUser) {
         return res.status(409).json({ message: "User already exists" });
-      }
+      };
+    
   
       // Create a new user
       const newUser = await User.create({
@@ -41,6 +42,11 @@ export const createUser = async (req, res) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
+      // Ensure utc_time is a Date object (parse it if it's not)
+    const utcTime = user.utc_time instanceof Date ? user.utc_time : new Date(user.utc_time);
+
+    // Format the UTC time to the desired format (e.g., "2023-09-07T21:46:17Z")
+    const formattedUtcTime = utcTime.toISOString();
   
       res.status(200).json({
         slack_name: user.slack_name,
@@ -48,7 +54,7 @@ export const createUser = async (req, res) => {
         current_day: user.current_day,
         github_file_url: user.github_file_url,
         github_repo_url: user.github_repo_url,
-        utc_time: user.utc_time,
+        utc_time: formattedUtcTime,
         status_code: 200,
       });
     } catch (error) {
